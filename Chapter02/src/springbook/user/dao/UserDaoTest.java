d2506118047bc6eac7380489db4d67116819d719
Jinkyu Han <hjk1019@gmail.com>
2020-07-16 21:21:53 +0900

    '스프링 테스트 적용'에 대한 내용 추가

--- Chapter02/src/springbook/user/dao/UserDaoTest.java
+++ Chapter02/src/springbook/user/dao/UserDaoTest.java
@@ -5,17 +5,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/applicationContext.xml")
+@ContextConfiguration(locations = "classpath:test-applicationContext.xml")
 public class UserDaoTest {
 
     @Autowired
