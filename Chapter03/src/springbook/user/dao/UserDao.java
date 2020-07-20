932c1e537c42effbfd92bce60619171d6d9218b4
Jinkyu Han <hjk1019@gmail.com>
2020-07-20 21:59:09 +0900

    '다시 보는 초난감 DAO'에 대한 내용 추가

--- Chapter03/src/springbook/user/dao/UserDao.java
+++ Chapter03/src/springbook/user/dao/UserDao.java
@@ -88,17 +88,42 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM users");
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("SELECT COUNT(*) FROM users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    // pass
+                }
+            }
 
-        rs.close();
-        ps.close();
-        c.close();
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    // pass
+                }
+            }
 
-        return count;
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    // pass
+                }
+            }
+        }
     }
  }
