ce5af1fb1bcc7947e93b26de46d5624eca245d7f
Jinkyu Han <hjk1019@gmail.com>
2020-08-03 20:31:21 +0900

    '스프링의 JDBCTEMPLATE'에 대한 내용 추가

--- Chapter03/src/springbook/user/dao/UserDao.java
+++ Chapter03/src/springbook/user/dao/UserDao.java
@@ -1,21 +1,26 @@
 package springbook.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
-
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
     }
+
+    private RowMapper<User> userMapper = (resultSet, rowNum) -> {
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+        return user;
+    };
 
     public void add(final User user) {
         this.jdbcTemplate.update(
@@ -23,17 +28,10 @@ public class UserDao {
             user.getId(), user.getName(), user.getPassword());
     }
 
-    public User get(String id) throws SQLException {
+    public User get(String id) {
         return this.jdbcTemplate.queryForObject(
             "SELECT * FROM users WHERE id = ?",
-            new Object[] { id },
-            (resultSet, rowNum) -> {
-                User user = new User();
-                user.setId(resultSet.getString("id"));
-                user.setName(resultSet.getString("name"));
-                user.setPassword(resultSet.getString("password"));
-                return user;
-            });
+            new Object[] { id }, userMapper);
     }
 
     public void deleteAll() {
@@ -45,14 +43,6 @@ public class UserDao {
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query(
-                "SELECT * FROM users ORDER BY id",
-                (resultSet, i) -> {
-                    User user = new User();
-                    user.setId(resultSet.getString("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setPassword(resultSet.getString("password"));
-                    return user;
-                });
+        return this.jdbcTemplate.query("SELECT * FROM users ORDER BY id", userMapper);
     }
 }
