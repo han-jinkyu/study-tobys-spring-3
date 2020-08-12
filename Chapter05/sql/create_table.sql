19a7d67e39c76af55fa82640a4c8d29fe8119a20
Jinkyu Han <hjk1019@gmail.com>
2020-08-12 21:26:17 +0900

    '사용자 레벨 관리 기능 추가'에 대한 내용 추가

--- Chapter05/sql/create_table.sql
+++ Chapter05/sql/create_table.sql
@@ -2,5 +2,8 @@
 CREATE TABLE users (
     id VARCHAR(10) PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
-    password VARCHAR(10) NOT NULL
+    password VARCHAR(10) NOT NULL,
+    level TINYINT NOT NULL,
+    login INT NOT NULL,
+    recommend INT NOT NULL
 );
