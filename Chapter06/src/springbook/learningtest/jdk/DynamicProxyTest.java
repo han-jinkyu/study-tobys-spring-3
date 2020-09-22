d2f7660a04b3153534bf436138c2a3d0c08c34da
Jinkyu Han <hjk1019@gmail.com>
2020-09-22 23:30:03 +0900

    '스프링의 프록시 팩토리 빈'에 대한 내용 추가

---
+++ Chapter06/src/springbook/learningtest/jdk/DynamicProxyTest.java
@@ -0,0 +1,83 @@
+package springbook.learningtest.jdk;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+import java.lang.reflect.Method;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DynamicProxyTest {
+
+    @Test
+    public void invokeMethod() throws Exception {
+        String name = "Spring";
+
+        // length()
+        assertThat(name.length(), is(6));
+
+        Method lengthMethod = String.class.getMethod("length");
+        assertThat((Integer)lengthMethod.invoke(name), is(6));
+
+        // charAt()
+        assertThat(name.charAt(0), is('S'));
+
+        Method charAtMethod = String.class.getMethod("charAt", int.class);
+        assertThat((Character) charAtMethod.invoke(name, 0), is('S'));
+    }
+
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby"), is("Hello Toby"));
+        assertThat(hello.sayHi("Toby"), is("Hi Toby"));
+        assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+    }
+
+    @Test
+    public void proxyFactoryBean() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+        pfBean.addAdvice(new UppercaseAdvice());
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+
+    private class UppercaseAdvice implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+            String ret = (String)methodInvocation.proceed();
+            return ret.toUpperCase();
+        }
+    }
+
+    public interface Hello {
+        String sayHello(String name);
+        String sayHi(String name);
+        String sayThankYou(String name);
+    }
+
+    public class HelloTarget implements Hello {
+        @Override
+        public String sayHello(String name) {
+            return "Hello " + name;
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return "Hi " + name;
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return "Thank You " + name;
+        }
+    }
+}
