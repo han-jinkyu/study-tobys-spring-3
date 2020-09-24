2d72c1bb902cb0fc805004fc38de20c4855e63af
Jinkyu Han <hjk1019@gmail.com>
2020-09-24 13:09:54 +0900

    '스프링의 프록시 팩토리 빈'에 대한 내용 추가

--- Chapter06/src/springbook/learningtest/jdk/DynamicProxyTest.java
+++ Chapter06/src/springbook/learningtest/jdk/DynamicProxyTest.java
@@ -4,6 +4,8 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 
@@ -56,6 +58,23 @@ public class DynamicProxyTest {
             String ret = (String)methodInvocation.proceed();
             return ret.toUpperCase();
         }
+    }
+
+    @Test
+    public void pointcutAdvisor() {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new HelloTarget());
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("sayH*");
+
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+
+        Hello proxiedHello = (Hello) pfBean.getObject();
+
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("Thank You Toby"));
     }
 
     public interface Hello {
