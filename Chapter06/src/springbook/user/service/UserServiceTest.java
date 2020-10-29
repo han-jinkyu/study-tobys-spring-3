b413c61f6d79feb9c1d31e4943902cf58eee8fef
Jinkyu Han <hjk1019@gmail.com>
2020-10-29 17:17:17 +0900

    '트랜잭션 지원 테스트'에 대한 내용 추가

--- Chapter06/src/springbook/user/service/UserServiceTest.java
+++ Chapter06/src/springbook/user/service/UserServiceTest.java
@@ -11,8 +11,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
@@ -39,9 +38,6 @@ public class UserServiceTest {
 
     @Autowired
     UserDao userDao;
-
-    @Autowired
-    PlatformTransactionManager transactionManager;
 
     List<User> users;   // 테스트 픽스쳐
 
@@ -162,16 +158,10 @@ public class UserServiceTest {
     }
 
     @Test
+    @Transactional
     public void transactionSync() {
-        DefaultTransactionDefinition txDefinition = new DefaultTransactionDefinition();
-        TransactionStatus txStatus = transactionManager.getTransaction(txDefinition);
-
-        try {
-            userService.deleteAll();
-            userService.add(users.get(0));
-            userService.add(users.get(1));
-        } finally {
-            transactionManager.rollback(txStatus);
-        }
+        userService.deleteAll();
+        userService.add(users.get(0));
+        userService.add(users.get(1));
     }
 }
