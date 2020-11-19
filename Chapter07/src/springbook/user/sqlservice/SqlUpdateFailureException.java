78cc0ff1c85342ed09479b1e0e40e93773aa1803
Jinkyu Han <hjk1019@gmail.com>
2020-11-19 19:52:08 +0900

    '인터페이스 상속을 통한 안전한 기능확장'에 대한 내용 추가

---
+++ Chapter07/src/springbook/user/sqlservice/SqlUpdateFailureException.java
@@ -0,0 +1,11 @@
+package springbook.user.sqlservice;
+
+public class SqlUpdateFailureException extends Exception {
+    public SqlUpdateFailureException(String message) {
+        super(message);
+    }
+
+    public SqlUpdateFailureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
