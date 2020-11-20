e500bd68cfed8f7c0328e4fd64224c8f900c01c5
Jinkyu Han <hjk1019@gmail.com>
2020-11-20 17:25:06 +0900

    '인터페이스 상속을 통한 안전한 기능확장'에 대한 내용 추가

---
+++ Chapter07/src/springbook/user/sqlservice/ConcurrentHashMapSqlRegistryTest.java
@@ -0,0 +1,60 @@
+package springbook.user.sqlservice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ConcurrentHashMapSqlRegistryTest {
+    UpdatableSqlRegistry sqlRegistry;
+
+    @Before
+    public void setUp() {
+        sqlRegistry = new ConcurrentHashMapSqlRegistry();
+        sqlRegistry.registerSql("KEY1", "SQL1");
+        sqlRegistry.registerSql("KEY2", "SQL2");
+        sqlRegistry.registerSql("KEY3", "SQL3");
+    }
+
+    @Test
+    public void find() throws SqlNotFoundException {
+        checkFindResult("SQL1", "SQL2", "SQL3");
+    }
+
+    private void checkFindResult(String expected1, String expected2, String expected3)
+            throws SqlNotFoundException {
+        assertThat(sqlRegistry.findSql("KEY1"), is(expected1));
+        assertThat(sqlRegistry.findSql("KEY2"), is(expected2));
+        assertThat(sqlRegistry.findSql("KEY3"), is(expected3));
+    }
+
+    @Test(expected = SqlNotFoundException.class)
+    public void unknownKey() throws SqlNotFoundException {
+        sqlRegistry.findSql("SQL999!@#$");
+    }
+
+    @Test
+    public void updateSingle() throws SqlUpdateFailureException, SqlNotFoundException {
+        sqlRegistry.updateSql("KEY2", "Modified2");
+        checkFindResult("SQL1", "Modified2", "SQL3");
+    }
+
+    @Test
+    public void updateMulti() throws SqlUpdateFailureException, SqlNotFoundException {
+        Map<String, String> sqlmap = new HashMap<>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY3", "Modified3");
+
+        sqlRegistry.updateSql(sqlmap);
+        checkFindResult("Modified1", "SQL2", "Modified3");
+    }
+
+    @Test(expected = SqlUpdateFailureException.class)
+    public void updateWithNotExistingKey() throws SqlUpdateFailureException {
+        sqlRegistry.updateSql("SQL9999!@#$", "Modified2");
+    }
+}
