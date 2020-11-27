7b89864d93f148e57ed82a92ceeb25f52a0ade25
Jinkyu Han <hjk1019@gmail.com>
2020-11-27 18:21:23 +0900

    'DI를 이용해 다양한 구현 방법 적용하기'에 대한 내용 추가

--- Chapter07/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ Chapter07/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -2,6 +2,10 @@ package springbook.user.sqlservice.updatable;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 import springbook.user.sqlservice.SqlNotFoundException;
 import springbook.user.sqlservice.SqlUpdateFailureException;
 import springbook.user.sqlservice.UpdatableSqlRegistry;
@@ -11,9 +15,11 @@ import java.util.Map;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     JdbcTemplate jdbcTemplate;
+    TransactionTemplate transactionTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
     }
 
     @Override
@@ -25,10 +31,15 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     }
 
     @Override
-    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-        for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
-            updateSql(entry.getKey(), entry.getValue());
-        }
+    public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+                    updateSql(entry.getKey(), entry.getValue());
+                }
+            }
+        });
     }
 
     @Override
--- Chapter07/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ Chapter07/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -1,14 +1,23 @@
 package springbook.user.sqlservice.updatable;
 
 import org.junit.After;
+import org.junit.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import springbook.user.sqlservice.AbstractUpdatableSqlRegistryTest;
+import springbook.user.sqlservice.SqlNotFoundException;
+import springbook.user.sqlservice.SqlUpdateFailureException;
 import springbook.user.sqlservice.UpdatableSqlRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.fail;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
     EmbeddedDatabase db;
+    EmbeddedDbSqlRegistry sqlRegistry;
 
     @Override
     protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
@@ -17,10 +26,26 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
                 .addScript("classpath:springbook/user/sqlservice/updatable/sqlRegistrySchema.sql")
                 .build();
 
-        EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
-        embeddedDbSqlRegistry.setDataSource(db);
+        sqlRegistry = new EmbeddedDbSqlRegistry();
+        sqlRegistry.setDataSource(db);
 
-        return embeddedDbSqlRegistry;
+        return sqlRegistry;
+    }
+
+    @Test
+    public void transactionalUpdate() throws SqlNotFoundException {
+        checkFindResult("SQL1", "SQL2", "SQL3");
+
+        Map<String, String> sqlmap = new HashMap<>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY9999!@#$", "Modified9999");
+
+        try {
+            sqlRegistry.updateSql(sqlmap);
+            fail();
+        } catch (SqlUpdateFailureException e) {}
+
+        checkFindResult("SQL1", "SQL2", "SQL3");
     }
 
     @After
