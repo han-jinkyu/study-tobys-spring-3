c14a63116b1e45795ef77f88876c343c7a31125c
Jinkyu Han <hjk1019@gmail.com>
2020-12-10 20:20:02 +0900

    '스프링 3.1의 DI'에 대한 내용 추가

--- Chapter07/src/springbook/user/AppContext.java
+++ Chapter07/src/springbook/user/AppContext.java
@@ -8,17 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springbook.user.dao.UserDao;
-import springbook.user.sqlservice.OxmSqlService;
-import springbook.user.sqlservice.SqlRegistry;
 import springbook.user.sqlservice.SqlService;
-import springbook.user.sqlservice.updatable.EmbeddedDbSqlRegistry;
 
 import javax.sql.DataSource;
 
